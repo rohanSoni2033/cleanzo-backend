6e9b95068f7ff02d46d5e3eb87ec3f83744198aa
rohan soni <rohansoni54476@gmail.com>
2023-04-06 13:45:38 +0530

    add updateMobileNumber method

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,30 @@
-import User from '../models/User.js';
+import User, { USER_TYPE } from '../models/User.js';
 import statusCode from '../utils/statusCode.js';
 import GlobalError from './../error/GlobalError.js';
 import asyncHandler from './../utils/asyncHandler.js';
 import jwt from 'jsonwebtoken';
 import errmsg from '../error/errorMessages.js';
-import { verifyMobileNumberUsingOTP, verifyOTP } from './verificationController.js';
+import {
+  verifyMobileNumberUsingOTP,
+  verifyOTP,
+} from './verificationController.js';
 
 // api/v1.0/login
 export const loginController = asyncHandler(async function (req, res, next) {
   const { mobileNumber } = req.body;
 
-  if (!mobileNumber || mobileNumber.length !== 10 || !mobileNumber.match(/^[0-9]*$/)) {
-    return next(new GlobalError(errmsg.INVALID_MOBILE_NUMBER, statusCode.BAD_REQUEST));
+  if (
+    !mobileNumber ||
+    mobileNumber.length !== 10 ||
+    !mobileNumber.match(/^[0-9]*$/)
+  ) {
+    return next(
+      new GlobalError(errmsg.INVALID_MOBILE_NUMBER, statusCode.BAD_REQUEST)
+    );
   }
 
-  const { hashedString, otpExpiresTimestamp } = await verifyMobileNumberUsingOTP(mobileNumber);
+  const { hashedString, otpExpiresTimestamp } =
+    await verifyMobileNumberUsingOTP(mobileNumber);
 
   res.status(statusCode.OK).json({
     status: 'success',
@@ -23,14 +33,40 @@ export const loginController = asyncHandler(async function (req, res, next) {
 });
 
 // api/v.1.0/verify-otp
-export const verifyOTPController = asyncHandler(async function (req, res, next) {
+export const verifyOTPController = asyncHandler(async function (
+  req,
+  res,
+  next
+) {
   const { mobileNumber, hashedString, otp, otpExpiresTimestamp } = req.body;
 
-  await validDataForOTPVerification(mobileNumber, hashedString, otp, otpExpiresTimestamp);
+  await validDataForOTPVerification(
+    mobileNumber,
+    hashedString,
+    otp,
+    otpExpiresTimestamp
+  );
 
-  let id = await User.getOne({ mobileNumber });
+  if (req.id && req.headers.token) {
+    await User.updateOneById(req.id, { mobileNumber });
+    return res.status(statusCode.OK).json({
+      status: 'success',
+    });
+  }
 
-  if (!id) id = await User.createOne(mobileNumber);
+  const user = await User.getOne({ mobileNumber });
+
+  let id;
+
+  if (user) {
+    id = user._id;
+  } else {
+    id = await User.createOne({
+      mobileNumber,
+      userType: USER_TYPE.USER,
+      createdAt: Date.now(),
+    });
+  }
 
   const token = jwt.sign({ id }, process.env.jwtSecret, {
     expiresIn: process.env.jwtExpiresTime,
@@ -45,10 +81,12 @@ export const verifyOTPController = asyncHandler(async function (req, res, next) 
 });
 
 // api/v1.0/auth/member/login
-export const memberLoginController = asyncHandler(async function (req, res, next) {
+export const memberLoginController = asyncHandler(async (req, res, next) => {
   const { mobileNumber, password } = req.body;
   if (!mobileNumber) {
-    return next(new GlobalError(errmsg.NO_MOBILE_NUMBER, statusCode.BAD_REQUEST));
+    return next(
+      new GlobalError(errmsg.NO_MOBILE_NUMBER, statusCode.BAD_REQUEST)
+    );
   }
 
   if (!password) {
@@ -57,7 +95,7 @@ export const memberLoginController = asyncHandler(async function (req, res, next
 
   const member = await User.getOne({
     mobileNumber: mobileNumber,
-    userType: "member"
+    userType: 'member',
   });
 
   if (!member) {
@@ -70,24 +108,30 @@ export const memberLoginController = asyncHandler(async function (req, res, next
     return next(new GlobalError(errmsg.WRONG_PASSWORD, statusCode.BAD_REQUEST));
   }
 
-  const { hashedString, otpExpiresTimestamp } = await verifyMobileNumberUsingOTP(mobileNumber);
+  const { hashedString, otpExpiresTimestamp } =
+    await verifyMobileNumberUsingOTP(mobileNumber);
 
   res.status(statusCode.OK).json({
-    status: "success",
+    status: 'success',
     data: {
       mobileNumber,
       otpExpiresTimestamp,
       hashedString,
-    }
-  })
+    },
+  });
 });
 
-const validDataForOTPVerification = async (mobileNumber, hashedString, otp, otpExpiresTimestamp) => {
+const validDataForOTPVerification = async (
+  mobileNumber,
+  hashedString,
+  otp,
+  otpExpiresTimestamp
+) => {
   if (!mobileNumber) {
     throw new GlobalError(
       errmsg.NO_MOBILE_NUMBER_PROVIDED,
       statusCode.UNAUTHORIZED
-    )
+    );
   }
 
   if (!hashedString || !otp || !otpExpiresTimestamp) {
@@ -97,17 +141,21 @@ const validDataForOTPVerification = async (mobileNumber, hashedString, otp, otpE
     );
   }
   await verifyOTP(mobileNumber, otp, otpExpiresTimestamp, hashedString);
-}
+};
 
 export const protectRoute = asyncHandler(async function (req, res, next) {
   const { token } = req.headers;
 
   if (!token) {
-    return next(new GlobalError(errmsg.TOKEN_NOT_DEFINED, statusCode.UNAUTHORIZED));
+    return next(
+      new GlobalError(errmsg.TOKEN_NOT_DEFINED, statusCode.UNAUTHORIZED)
+    );
   }
 
   if (!token.startsWith('Token')) {
-    return next(new GlobalError(errmsg.TOKEN_NOT_DEFINED, statusCode.UNAUTHORIZED));
+    return next(
+      new GlobalError(errmsg.TOKEN_NOT_DEFINED, statusCode.UNAUTHORIZED)
+    );
   }
 
   const tokenString = token.split(' ')[1];
@@ -118,10 +166,13 @@ export const protectRoute = asyncHandler(async function (req, res, next) {
   }
 
   const { id } = decodedTokenString;
-  const user = await User.findById(id);
+
+  const user = await User.getOneById(id);
 
   if (!user) {
-    return next(new GlobalError(errmsg.USER_NOT_FOUND, statusCode.UNAUTHORIZED));
+    return next(
+      new GlobalError(errmsg.USER_NOT_FOUND, statusCode.UNAUTHORIZED)
+    );
   }
 
   req['id'] = user._id.toString();
@@ -130,13 +181,62 @@ export const protectRoute = asyncHandler(async function (req, res, next) {
   next();
 });
 
-export const giveAccessPermission = (...userType) => {
-  return asyncHandler((req, res, next) => {
-    userType.forEach(user => {
-      if (user === req['user-type']) {
-        return next();
-      }
-      return next(new GlobalError(errmsg.UNAUTHORIZED_ACCESS, statusCode.FORBIDDEN));
-    });
+export const accessPermission = (...userType) => {
+  return asyncHandler(async (req, res, next) => {
+    if (userType.includes(req['user-type'])) {
+      return next();
+    }
+    return next(
+      new GlobalError(errmsg.UNAUTHORIZED_ACCESS, statusCode.FORBIDDEN)
+    );
   });
 };
+
+export const updateMobileNumber = asyncHandler(async (req, res, next) => {
+  const { newMobileNumber } = req.body;
+
+  if (!newMobileNumber) {
+    return next(
+      new GlobalError(
+        'Please provide a new mobile number to updater',
+        statusCode.BAD_REQUEST
+      )
+    );
+  }
+
+  const currentUser = await User.getOneById(req.id);
+
+  if (!currentUser) {
+    return next(
+      new GlobalError(
+        'User not found with this mobile number, try again',
+        statusCode.BAD_REQUEST
+      )
+    );
+  }
+
+  const isMobileNumberExits = await User.getOne({
+    mobileNumber: newMobileNumber,
+  });
+
+  if (isMobileNumberExits) {
+    return next(
+      new GlobalError(
+        'mobile number is already in use, please try another',
+        statusCode.BAD_REQUEST
+      )
+    );
+  }
+
+  const { hashedString, otpExpiresTimestamp } =
+    await verifyMobileNumberUsingOTP(newMobileNumber);
+
+  res.status(statusCode.OK).json({
+    status: 'success',
+    data: {
+      mobileNumber: newMobileNumber,
+      otpExpiresTimestamp,
+      hashedString,
+    },
+  });
+});
