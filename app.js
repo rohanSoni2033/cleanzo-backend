642f0b721f77d64ce37708efbdcb24c15d660ebf
rohan soni <rohansoni54476@gmail.com>
2023-04-06 13:46:28 +0530

    add me routers

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from 'express';
-import authRouters from "./routers/authRouters.js";
-import servicesRouters from "./routers/serviceRouters.js";
-import userRouters from "./routers/userRouter.js";
+import authRouters from './routers/authRouters.js';
+import servicesRouters from './routers/serviceRouters.js';
+import userRouters from './routers/userRouter.js';
+import meRouter from './routers/meRouters.js';
+import GlobalError from './error/GlobalError.js';
+import statusCode from './utils/statusCode.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1.0/auth', authRouters);
-app.use("/api/v1.0/services", servicesRouters);
-app.use("/api/v1.0/users", userRouters);
+app.use('/api/v1.0/services', servicesRouters);
+app.use('/api/v1.0/users', userRouters);
+app.use('/api/v1.0/me', meRouter);
+
+app.use('*', (req, res, next) => {
+  return next(new GlobalError('invalid route request', statusCode.BAD_REQUEST));
+});
 
 app.use((err, req, res, next) => {
   res.status(400).json({
