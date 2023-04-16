c44175842d6ef4854850450dc328fb3de410f0f8
rohan soni <rohansoni54476@gmail.com>
2023-04-16 10:45:49 +0530

    refactor server and app file

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import sanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+
+import GlobalError from './error/GlobalError.js';
+import statusCode from './utils/statusCode.js';
+
 import authRouters from './routers/authRouters.js';
 import servicesRouters from './routers/serviceRouters.js';
 import userRouters from './routers/userRouter.js';
 import meRouter from './routers/meRouters.js';
-import GlobalError from './error/GlobalError.js';
-import statusCode from './utils/statusCode.js';
+import slotRouters from './routers/slotRouters.js';
+import bookingRouters from './routers/bookingRouter.js';
+import vehicleRouters from './routers/vehicleRouter.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(sanitize());
+app.use(xss());
 app.use(express.json());
+
+const limit = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this ip, try again after sometime',
+});
+
+app.use('/api', limit);
+app.use(express.json({ limit: '10kb' }));
 
 app.use('/api/v1.0/auth', authRouters);
 app.use('/api/v1.0/services', servicesRouters);
 app.use('/api/v1.0/users', userRouters);
 app.use('/api/v1.0/me', meRouter);
+app.use('/api/v1.0/slots', slotRouters);
+app.use('/api/v1.0/bookings', bookingRouters);
+// app.use('/api/v1.0/membership', membershipRouters);
+app.use('/api/v1.0/vehicles', vehicleRouters);
 
 app.use('*', (req, res, next) => {
-  return next(new GlobalError('invalid route request', statusCode.BAD_REQUEST));
+  return next(
+    new GlobalError(`${req.baseUrl} not found`, statusCode.BAD_REQUEST)
+  );
 });
 
 app.use((err, req, res, next) => {
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import app from './app.js';
 const port = process.env.PORT || process.env.localPort;
 
 app.listen(port, () => {
-  console.log(`🚀 server is running at port ${port} in ${process.env.NODE_ENV} environment`);
+  console.log(
+    `🚀 server is running at port ${port} in ${process.env.NODE_ENV} environment`
+  );
 });
 
-
-process.on("uncaughtException", (err) => {
-  console.log(err.message);
-})
+process.on('uncaughtException', err => {
+  console.log(err);
+});
