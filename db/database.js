884783aacd84d1d10cba97a7f8a6a6bcfd2f88ee
rohan soni <rohansoni54476@gmail.com>
2023-04-16 10:48:44 +0530

    refactor database

--- db/database.js
+++ db/database.js
@@ -1,22 +1,25 @@
 import * as dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
 import { MongoClient } from 'mongodb';
 
 // connection uri for connecting with the mongodb database
-const connectionUri = process.env.connectionUriHosted.replace("${PASSWORD}", process.env.databasePassword).replace("${USERNAME}", process.env.databaseUsername);
+// const connectionUri = process.env.connectionUriHosted.replace("${PASSWORD}", process.env.databasePassword).replace("${USERNAME}", process.env.databaseUsername);
+
+// const connectionUri = 'mongodb://127.0.0.1:27017';
+const connectionUri = 'mongodb://127.0.0.1:27017';
 
 const databaseName = process.env.database;
 
-const connect = async function () {
+const main = async function () {
   const mongoClient = new MongoClient(connectionUri);
   try {
     const dbClient = await mongoClient.connect();
-    console.log("🚀 successfully connected with the database");
+    console.log('🚀 successfully connected with the database');
     return dbClient.db(databaseName);
   } catch (err) {
-    console.log("something went wrong with the database ❌");
+    console.log('something went wrong with the database ❌');
   }
 };
 
-export default await connect();
+export default await main();
