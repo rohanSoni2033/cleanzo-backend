9aceb90d83daacde26cfc2b03b06fc4689f555d8
rohan soni <rohansoni54476@gmail.com>
2023-04-29 06:56:32 +0530

    add the collections.js

---
+++ db/collections.js
@@ -0,0 +1,13 @@
+import database from './database.js';
+
+export const ServiceCategory = database.collection('serviceCategories');
+export const Service = database.collection('services');
+export const Vehicle = database.collection('vehicles');
+
+export const Booking = database.collection('bookings');
+export const User = database.collection('users');
+export const Slot = database.collection('slots');
+
+export const MembershipPlan = database.collection('membershipPlans');
+export const Membership = database.collection('membership');
+export const MembershipBooking = database.collection('membershipBookings');
