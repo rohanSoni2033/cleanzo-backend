31e46c5706025e445f1fe3ebf8fa00dbd128702d
rohan soni <rohansoni54476@gmail.com>
2023-04-29 11:55:48 +0530

    add a file for constants

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+export const USER_TYPE = {
+  ADMIN: 'admin',
+  MEMBER: 'member',
+  USER: 'user',
+};
+
+export const BookingStatus = {
+  PENDING: 'pending',
+  COMPLETED: 'completed',
+  FAILED: 'failed',
+  CANCELED: 'canceled',
+};
+
+export const MAXIMUM_BOOKING_PER_SLOT = 3;
