24bff55e142554e237d423789e90d94c190baa5c
rohan soni <rohansoni54476@gmail.com>
2023-05-09 14:26:57 +0530

    delete serviceGroups file

--- controllers/serviceGroupController.js
+++
@@ -1,7 +0,0 @@
-import ServiceGroup from '../models/ServiceGroup.js';
-import { deleteOne, getAll, getOne, updateOne } from './factoryController.js';
-
-export const getAllServiceGroups = getAll(ServiceGroup);
-export const getServiceGroup = getOne(ServiceGroup);
-export const updateServiceGroup = updateOne(ServiceGroup);
-export const deleteServiceGroup = deleteOne(ServiceGroup);
