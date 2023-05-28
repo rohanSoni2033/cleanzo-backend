87236c1d39af25f83e6952573c6faa2c4b7f82d1
rohan soni <rohansoni54476@gmail.com>
2023-05-29 01:44:10 +0530

    add new collections

--- db/collections.js
+++ db/collections.js
@@ -9,7 +9,7 @@ export const User = database.collection('users');
 export const Slot = database.collection('slots');
 
 export const MembershipPlan = database.collection('membershipPlans');
-export const Membership = database.collection('membership');
-export const MembershipBooking = database.collection('membershipBookings');
+export const Membership = database.collection('memberships');
+export const Payments = database.collection('payments');
 
 export const BusinessRelated = database.collection('businessRelated');
